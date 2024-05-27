622a03e20bdfe2f03627100feff91a7b2cf50540
Luiz Gonzaga <112145713+Luizribeiro05@users.noreply.github.com>
2024-05-27 01:32:09 -0300

    aula06

--- aula06/src/App.jsx
+++ aula06/src/App.jsx
@@ -1,27 +1,35 @@
-import React from 'react'
-import { BrowserRouter , Routes , Route } from 'react-router-dom'
-import Afaculdade from './pages/Afaculdade'
-import DpoLgpd from './pages/DpoLgpd'
-import Inicial from './pages/Inicial'
-import Noticias from './pages/Noticias'
-
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Afaculdade from './pages/Afaculdade';
+import DpoLgpd from './pages/DpoLgpd';
+import Inicial from './pages/Inicial';
+import Noticias from './pages/Noticias';
+import VisualizaNoticia from './pages/VisualizaNoticia';
+import './styles/styles.css';
+import NoticiasAxios from './pages/NoticiasAxios';
 
 const App = () => {
   return (
     <>
-    <BrowserRouter>
-    <img src='uniesp.jpg' />
-    <h1>Titulo da Pagina</h1>
-    <Routes>
-      <Route path='/' element={<Inicial />} />
-      <Route path='/a-faculdade' element={<Afaculdade />} />
-      <Route path='/dpo-lgpd' element={<DpoLgpd />} />
-      <Route path='/noticias' element={<Noticias />} />
-    </Routes>
-    </BrowserRouter>
-    <div>App</div>
+      <BrowserRouter>
+        <header>
+          <img src='uniesp.jpg' width={100} height={100} alt="Uniesp logo"/>
+          <h1>Uniesp</h1>
+        </header>
+        <Navbar/>
+        <main>
+          <Routes>
+            <Route path='/' element={<Inicial />} />
+            <Route path='/a-faculdade' element={<Afaculdade />} />
+            <Route path='/dpo-lgpd' element={<DpoLgpd />} />
+            <Route path='/noticias' element={<NoticiasAxios />} />
+            <Route path='/visualiza-noticia/:id' element={<VisualizaNoticia/>} />
+          </Routes>
+        </main>
+      </BrowserRouter>
     </>
- )
+  );
 }
 
-export default App
+export default App;
--- aula06/src/pages/NoticiasAxios.jsx
+++ aula06/src/pages/NoticiasAxios.jsx
@@ -1,18 +1,21 @@
+import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const url = "http://localhost:3000/noticias"
 
-const Noticias = () => {
+const NoticiasAxios = () => {
 
   const [noticias,setNoticias] = useState([])
   
   useEffect(() => {
     async function fetchData(){
-      const res = await fetch(url)
-      const data = await res.json()
-
-      setNoticias(data)
+      try {
+        const res = await axios.get(url)
+        setNoticias(res.data)
+      } catch(error){
+        console.error("Erro ao buscar as notícias: ", error)
+      }
     }
     fetchData()
   },  [])
@@ -34,4 +37,4 @@ const Noticias = () => {
   )
 }
 
-export default Noticias
+export default NoticiasAxios
